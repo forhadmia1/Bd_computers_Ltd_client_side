c16ba28a53c2ccac68a9c6cda0b8ce3edea43a26
forhadmia1 <forhad8613@gmail.com>
2022-05-22 03:07:30 +0600

    business summery section done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../Shared/Footer';
 import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <BusinessSummary />
             <Footer />
         </div>
     );
