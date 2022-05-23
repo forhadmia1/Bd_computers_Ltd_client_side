5e6db9c308d56413cb4cef6e266efc8814e6fab6
forhadmia1 <forhad8613@gmail.com>
2022-05-24 01:47:34 +0600

    implement purchase page

--- src/Pages/Home/PurchaseHardware.js
+++ src/Pages/Home/PurchaseHardware.js
@@ -15,7 +15,7 @@ const PurchaseHardware = () => {
             .then(res => res.json())
             .then(data => setItem(data))
     }, [id])
-    const { name, image, description, avaliable_quantity, minimum_quantity, unit_price } = item;
+    const { name, image, description, available_quantity, minimum_quantity, unit_price } = item;
     const [quantity, setQuantity] = useState(minimum_quantity)
 
     if (loading) {
@@ -30,87 +30,60 @@ const PurchaseHardware = () => {
         console.log(address, phone, quantity)
 
     }
-
-
-
-
-
     return (
-        <div class="container my-24 px-6 mx-auto">
-            <section class="mb-32 text-gray-800 text-center md:text-left">
-                <div class="block rounded-lg shadow-lg bg-white">
-                    <div class="flex flex-wrap items-center">
-                        <div class="grow-0 shrink-0 basis-auto block lg:flex w-full lg:w-6/12 xl:w-4/12">
-                            <img src={image} alt={name}
-                                class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" />
-                        </div>
-                        <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
-                            <div class="px-6 pt-12 md:px-12">
-                                <h2 class="text-3xl font-bold mb-6 pb-2">{name}</h2>
-                                <p class="text-gray-500 mb-6 pb-2">
-                                    {description}
-                                </p>
-                                <div class="flex flex-wrap mb-6">
-                                    <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
-                                        <p class="flex items-center justify-center md:justify-start">
-                                            <BsFillCheckCircleFill className='mr-2' />Available Quantity: {avaliable_quantity}
-                                        </p>
-                                    </div>
-                                    <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
-                                        <p class="flex items-center justify-center md:justify-start">
-                                            <BsFillCheckCircleFill className='mr-2' />Minimum Quantity: {minimum_quantity}
-                                        </p>
-                                    </div>
-                                    <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
-                                        <p class="flex items-center justify-center md:justify-start">
-                                            <BsFillCheckCircleFill className='mr-2' />Unit Price: {unit_price}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='px-12'>
-                                <form onSubmit={handleForm}>
-                                    <div class="form-control w-full max-w-md">
-                                        <label class="label">
-                                            <span class="label-text">Name:</span>
-                                        </label>
-                                        <input type="text" value={user.displayName} disabled class="input input-bordered w-full max-w-md" />
-                                    </div>
-                                    <div class="form-control w-full max-w-md">
-                                        <label class="label">
-                                            <span class="label-text">Email:</span>
-                                        </label>
-                                        <input type="text" value={user.email} disabled class="input input-bordered w-full max-w-md" />
-                                    </div>
-                                    <div class="form-control w-full max-w-md">
-                                        <label class="label">
-                                            <span class="label-text">Address:</span>
-                                        </label>
-                                        <input name='address' type="text" placeholder="Address" class="input input-bordered w-full max-w-md" required />
-                                    </div>
-                                    <div class="form-control w-full max-w-md">
-                                        <label class="label">
-                                            <span class="label-text">Phone:</span>
-                                        </label>
-                                        <input name='phone' type="text" placeholder="Phone number" class="input input-bordered w-full max-w-md" required />
-                                    </div>
-                                    <div class="form-control w-full max-w-md">
-                                        <label class="label">
-                                            <span class="label-text">Quantity:</span>
-                                        </label>
-                                        <input name='quantity' value={quantity} type="number" placeholder="Input quantity" class="input input-bordered w-full max-w-md" />
-                                        {/* <label class="label">
+        <div className='grid grid-cols-2 container mx-auto mt-4 px-12 gap-20'>
+            <div class="card w-full bg-base-100 shadow-xl">
+                <figure><img className='w-full' src={image} alt={name} /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{name}</h2>
+                    <p>{description}</p>
+                    <p className='flex items-center'><BsFillCheckCircleFill className='mr-2' />Avaliable Quantity: {available_quantity}</p>
+                    <p className='flex items-center'><BsFillCheckCircleFill className='mr-2' />Minimum Quantity: {minimum_quantity}</p>
+                    <p className='flex items-center'><BsFillCheckCircleFill className='mr-2' />Unit Price: ${unit_price}</p>
+
+
+                </div>
+            </div>
+            <div>
+                <h2 className='text-2xl font-bold text-accent'>Order Here</h2>
+                <form onSubmit={handleForm}>
+                    <div class="form-control w-full max-w-md">
+                        <label class="label">
+                            <span class="label-text">Name:</span>
+                        </label>
+                        <input type="text" value={user.displayName} disabled class="input input-bordered w-full max-w-md" />
+                    </div>
+                    <div class="form-control w-full max-w-md">
+                        <label class="label">
+                            <span class="label-text">Email:</span>
+                        </label>
+                        <input type="text" value={user.email} disabled class="input input-bordered w-full max-w-md" />
+                    </div>
+                    <div class="form-control w-full max-w-md">
+                        <label class="label">
+                            <span class="label-text">Address:</span>
+                        </label>
+                        <input name='address' type="text" placeholder="Address" class="input input-bordered w-full max-w-md" required />
+                    </div>
+                    <div class="form-control w-full max-w-md">
+                        <label class="label">
+                            <span class="label-text">Phone:</span>
+                        </label>
+                        <input name='phone' type="text" placeholder="Phone number" class="input input-bordered w-full max-w-md" required />
+                    </div>
+                    <div class="form-control w-full max-w-md">
+                        <label class="label">
+                            <span class="label-text">Quantity:</span>
+                        </label>
+                        <input name='quantity' value={quantity} type="number" placeholder="Input quantity" class="input input-bordered w-full max-w-md" />
+                        {/* <label class="label">
                                             <span class="label-text text-red-500">{quantityError}</span>
                                         </label> */}
-                                    </div>
-                                    <input type="submit" value={'Order Now'} className='btn btn-primary my-6' />
-                                </form>
-
-                            </div>
-                        </div>
                     </div>
-                </div>
-            </section>
+                    <input type="submit" value={'Order Now'} className='btn btn-primary my-6' />
+                </form>
+
+            </div>
         </div>
     );
 };
