57f3bdf0b7ce3c8c42cb16a35b9fe6311e229b72
forhadmia1 <forhad8613@gmail.com>
2022-05-25 16:16:50 +0600

    change btn style

--- src/Pages/Dashboard/ManageAllOrderRow.js
+++ src/Pages/Dashboard/ManageAllOrderRow.js
@@ -22,7 +22,7 @@ const ManageAllOrderRow = ({ order, index, getData, refetch }) => {
             <td>{name}</td>
             <td>{quantity}</td>
             <td>{orderStatus === "paid" ? <span className='text-xl text-primary'>Pending</span> : <span className={`text-xl text-accent ${orderStatus === "unpaid" && 'text-red-500'}`}>{orderStatus}</span>}</td>
-            <td>{orderStatus === 'unpaid' ? <label onClick={() => getData({ _id, name })} for="cancel_order" class="btn modal-button btn-error">Delete Order</label> : orderStatus === 'paid' ? <button onClick={() => handleDeliver(_id)} className="btn btn-md btn-accent">Deliver</button> : <span className='text-xl font-bold text-primary'>Delivered</span>}</td>
+            <td>{orderStatus === 'unpaid' ? <label onClick={() => getData({ _id, name })} for="cancel_order" class="btn modal-button btn-error">Delete Order</label> : orderStatus === 'paid' ? <button onClick={() => handleDeliver(_id)} className="btn btn-md btn-accent w-full">Deliver</button> : <span className='text-xl font-bold text-primary'>Delivered</span>}</td>
         </tr >
     );
 };
